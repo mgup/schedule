156929241f9ff6b932ecc0e2827d320fca5959f0
U-SCHEDULE\disp <disp@schedule.(none)>
2013-07-01 15:52:38 +0400

    автоматический бэкап  1 июл 2013 г. 15:52:38

---
+++ autogit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/disp/documents/Расписание && git add .
+cd /home/disp/documents/Расписание && git commit -m "автоматический 
+бэкап 
+`date`"
+cd /home/disp/documents/Расписание && git push
