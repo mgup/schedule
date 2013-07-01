700e9e196d1d9e0fccc448c8ec56d1f027ccb789
Vitaly Shlyaga <storkvist@storkvist.net>
2013-07-01 16:55:59 +0400

    автоматический бэкап  1 июл 2013 г. 16:55:59

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 cd /home/disp/documents/Расписание && git add .
-cd /home/disp/documents/Расписание && git commit -m "автоматический 
-бэкап 
-`date`"
+cd /home/disp/documents/Расписание && git commit -m "автоматический бэкап `date`"
 cd /home/disp/documents/Расписание && git push
