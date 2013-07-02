ad0dc7d57cd3b5115355856074de4d30311d67c4
Vitaly Shlyaga <storkvist@storkvist.net>
2013-07-02 17:05:02 +0400

    автоматический бэкап Tue Jul  2 17:05:02     2013

--- autogit.sh
+++ autogit.sh
@@ -2,4 +2,5 @@
 
 cd /home/disp/documents/Расписание && git add .
 cd /home/disp/documents/Расписание && git commit -m "автоматический бэкап `date`"
-cd /home/disp/documents/Расписание && git push
+git config --global core.gitproxy http://disp:qwerty@192.168.200.41:3128
+cd /home/disp/documents/Расписание && git push origin master
